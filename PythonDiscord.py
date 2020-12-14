d599d9937f476bd76a1abbd4bb071235c0b96b1d
Client <65496622+tneilc@users.noreply.github.com>
2020-12-14 20:20:37 +0300

    Update PythonDiscord.py

--- PythonDiscord.py
+++ PythonDiscord.py
@@ -1,5 +1,4 @@
 import requests
-import json
 data = {"content":"Message you want to send"}
 header= {"authorization":"Your Token Here"}
 r = requests.post("https://discord.com/api/v8/channels/765972823595417660/messages",headers=header,data=data)
