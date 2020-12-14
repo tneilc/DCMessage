ca8123cbd2830e3802978787f73332632833dcf7
Client <65496622+tneilc@users.noreply.github.com>
2020-12-14 20:24:37 +0300

    Update PythonDiscord.py

--- PythonDiscord.py
+++ PythonDiscord.py
@@ -3,3 +3,9 @@ data = {"content":"{message}"}
 header= {"authorization":"{token}"}
 r = requests.post("https://discord.com/api/v8/channels/{channelid}/messages",headers=header,data=data)
 
+
+'''
+message = Message to send
+token = Your discord token
+channelid = Channel id of the channel to send message
+'''
